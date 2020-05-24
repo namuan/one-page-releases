0faff408c4e98fdb3b4e0775abe9812df3e831af
namuan <575441+namuan@users.noreply.github.com>
2020-05-24 22:23:03 +0100

    Combined signing in a single script

--- mac-codesign.sh
+++ mac-codesign.sh
@@ -1,3 +1,16 @@
+echo "###### OnePage Certificate ###############"
+
+export CERTIFICATE_P12=Certificate.p12;
+echo $OSX_CERTIFICATE_P12 | base64 --decode > $CERTIFICATE_P12;
+
+export KEYCHAIN=build.keychain;
+security create-keychain -p $OSX_CERTIFICATE_P12_PWD $KEYCHAIN;
+security default-keychain -s $KEYCHAIN;
+security unlock-keychain -p $OSX_CERTIFICATE_P12_PWD $KEYCHAIN;
+
+security import $CERTIFICATE_P12 -k $KEYCHAIN -P $OSX_CERTIFICATE_P12_PWD -T /usr/bin/codesign;
+security set-key-partition-list -S apple-tool:,apple: -s -k $OSX_CERTIFICATE_P12_PWD $KEYCHAIN
+
 echo "###### OnePage Code Signing ###############"
 
 ID="Developer ID Application: Nauman Leghari (Z8242PZ338)"
@@ -30,3 +43,6 @@ codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app
 
 # Add verification
 # spctl --raw -vvv --assess --type exec dist/OnePage.app
+
+# remove certs
+rm -fr *.p12

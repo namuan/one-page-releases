eeb2c58d41b1c233875f3a1596d4debc4d252414
namuan <575441+namuan@users.noreply.github.com>
2020-05-24 01:00:37 +0100

    Release 0.4.0
    - Adding code signing for MacOS

---
+++ import_cert.sh
@@ -0,0 +1,7 @@
+export CERTIFICATE_P12=Certificate.p12;
+echo $OSX_CERTIFICATE_P12 | base64 — decode > $CERTIFICATE_P12;
+export KEYCHAIN=build.keychain;
+security create-keychain -p $OSX_CERTIFICATE_P12_PWD $KEYCHAIN;
+security default-keychain -s $KEYCHAIN;
+security unlock-keychain -p $OSX_CERTIFICATE_P12_PWD $KEYCHAIN;
+security import $CERTIFICATE_P12 -k $KEYCHAIN -P $OSX_CERTIFICATE_P12_PWD -T /usr/bin/codesign;

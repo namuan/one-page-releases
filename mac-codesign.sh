0da77bf076d3f12f88e20819fd41c51e6f1776f3
namuan <575441+namuan@users.noreply.github.com>
2020-05-24 22:11:54 +0100

    Enabling code-signing for all files

--- mac-codesign.sh
+++ mac-codesign.sh
@@ -9,24 +9,24 @@ done
 
 for filename in $(find dist/OnePage.app -name "*.so"); do
     echo "Signing $filename"
-    # codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
+    codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
 done
 
 for filename in $(find dist/OnePage.app/Contents/MacOS -name "Qt*"); do
     echo "Signing $filename"
-    # codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
+    codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
 done
 
 for filename in $(ls dist/OnePage.app/Contents/MacOS/PyQt5/Qt/translations/*); do
     echo "Signing $filename"
-    # codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
+    codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
 done
 
-# codesign -v -f -s "$ID" /Users/nmn/workspace/one-page/dist/OnePage.app/Contents/MacOS/base_library.zip
+codesign -v -f -s "$ID" /Users/nmn/workspace/one-page/dist/OnePage.app/Contents/MacOS/base_library.zip
 
-# codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/python
-# codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/app
-# codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app
+codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/python
+codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/app
+codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app
 
 # Add verification
 # spctl --raw -vvv --assess --type exec dist/OnePage.app

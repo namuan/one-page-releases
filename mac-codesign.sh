7b737098bffcc82674b44093d2f04ba7f167c04a
namuan <575441+namuan@users.noreply.github.com>
2020-05-24 19:39:36 +0100

    Full path to codesign

--- mac-codesign.sh
+++ mac-codesign.sh
@@ -4,29 +4,29 @@ ID="Developer ID Application: Nauman Leghari (Z8242PZ338)"
 
 for filename in $(find dist/OnePage.app/ -name "*.dylib"); do
 	echo "Signing $filename"
-    codesign -v -f -s "$ID" $filename
+    /usr/bin/codesign -v -f -s "$ID" $filename
 done
 
 for filename in $(find dist/OnePage.app/ -name "*.so"); do
     echo "Signing $filename"
-    codesign -v -f -s "$ID" $filename
+    /usr/bin/codesign -v -f -s "$ID" $filename
 done
 
 for filename in $(find dist/OnePage.app/Contents/MacOS -name "Qt*"); do
     echo "Signing $filename"
-    codesign -v -f -s "$ID" $filename
+    /usr/bin/codesign -v -f -s "$ID" $filename
 done
 
 for filename in $(ls dist/OnePage.app/Contents/MacOS/PyQt5/Qt/translations/*); do
     echo "Signing $filename"
-    codesign -v -f -s "$ID" $filename
+    /usr/bin/codesign -v -f -s "$ID" $filename
 done
 
-codesign -v -f -s "$ID" /Users/nmn/workspace/one-page/dist/OnePage.app/Contents/MacOS/base_library.zip
+/usr/bin/codesign -v -f -s "$ID" /Users/nmn/workspace/one-page/dist/OnePage.app/Contents/MacOS/base_library.zip
 
-codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/python
-codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/app
-codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app
+/usr/bin/codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/python
+/usr/bin/codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/app
+/usr/bin/codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app
 
 # Add verification
 # spctl --raw -vvv --assess --type exec dist/OnePage.app

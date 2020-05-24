d645519a296fb479bfdd03ee575515d040bd3cd1
namuan <575441+namuan@users.noreply.github.com>
2020-05-24 20:44:12 +0100

    Update command and removing generated cert on file system

--- import_cert.sh
+++ import_cert.sh
@@ -5,3 +5,9 @@ security create-keychain -p $OSX_CERTIFICATE_P12_PWD $KEYCHAIN;
 security default-keychain -s $KEYCHAIN;
 security unlock-keychain -p $OSX_CERTIFICATE_P12_PWD $KEYCHAIN;
 security import $CERTIFICATE_P12 -k $KEYCHAIN -P $OSX_CERTIFICATE_P12_PWD -T /usr/bin/codesign;
+security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEYCHAIN
+
+./mac-codesign.sh
+
+# remove certs
+rm -fr *.p12

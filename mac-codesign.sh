2538a95e760c47663e820fd8b0ecb13f55a88a5d
namuan <575441+namuan@users.noreply.github.com>
2020-05-24 19:53:39 +0100

    Reverted path to codesign and added identity

--- mac-codesign.sh
+++ mac-codesign.sh
@@ -2,31 +2,31 @@ echo "###### OnePage Code Signing ###############"
 
 ID="Developer ID Application: Nauman Leghari (Z8242PZ338)"
 
-for filename in $(find dist/OnePage.app/ -name "*.dylib"); do
+for filename in $(find dist/OnePage.app -name "*.dylib"); do
 	echo "Signing $filename"
-    /usr/bin/codesign -v -f -s "$ID" $filename
+    codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
 done
 
-for filename in $(find dist/OnePage.app/ -name "*.so"); do
+for filename in $(find dist/OnePage.app -name "*.so"); do
     echo "Signing $filename"
-    /usr/bin/codesign -v -f -s "$ID" $filename
+    codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
 done
 
 for filename in $(find dist/OnePage.app/Contents/MacOS -name "Qt*"); do
     echo "Signing $filename"
-    /usr/bin/codesign -v -f -s "$ID" $filename
+    codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
 done
 
 for filename in $(ls dist/OnePage.app/Contents/MacOS/PyQt5/Qt/translations/*); do
     echo "Signing $filename"
-    /usr/bin/codesign -v -f -s "$ID" $filename
+    codesign -v -f -i "dev.deskriders.onepage" -s "$ID" $filename
 done
 
-/usr/bin/codesign -v -f -s "$ID" /Users/nmn/workspace/one-page/dist/OnePage.app/Contents/MacOS/base_library.zip
+codesign -v -f -s "$ID" /Users/nmn/workspace/one-page/dist/OnePage.app/Contents/MacOS/base_library.zip
 
-/usr/bin/codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/python
-/usr/bin/codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/app
-/usr/bin/codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app
+codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/python
+codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app/Contents/MacOS/app
+codesign -v -f --entitlements entitlements.plist -s "$ID" dist/OnePage.app
 
 # Add verification
 # spctl --raw -vvv --assess --type exec dist/OnePage.app
